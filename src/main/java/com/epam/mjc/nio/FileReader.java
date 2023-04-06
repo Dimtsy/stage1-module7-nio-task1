fc1d1d60b3bfa4d12d46b3c88bb2e9446a84de69
Dzmitry Tsybulski <dzmitrytsybulski@gmail.com>
2023-04-06 14:16:39 +0300

    Stage 1 Task NIO Task 1  Correction 1

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -2,32 +2,28 @@ package com.epam.mjc.nio;
 
 import java.io.*;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        try {
-            Path writeFilePath = Paths.get(String.valueOf(file));
-            String content = Files.readString(writeFilePath);
-
-            String[] subStr = content.split("\r\n");
-            String[][] subStrTwo = new String[subStr.length][2];
-            for (int i = 0; i < subStr.length; i++) {
-                subStrTwo[i] = subStr[i].split(": ");
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(String.valueOf(file)))) {
+            String line;
+            String[][] subStr = new String[reader.read()][];
+            int i = 0;
+            while ((line = reader.readLine()) != null) {
+                subStr[i] = line.split(": ");
+                i++;
             }
-
-            return new Profile(subStrTwo[0][1], Integer.parseInt(subStrTwo[1][1]),
-                    subStrTwo[2][1], Long.valueOf(subStrTwo[3][1]));
-
+            return new Profile(subStr[0][1], Integer.parseInt(subStr[1][1]),
+                    subStr[2][1], Long.valueOf(subStr[3][1]));
         } catch (IOException e) {
             e.printStackTrace();
         }
         return new Profile();
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         File file = new File("src\\main\\resources\\Profile.txt");
         FileReader fileReader = new FileReader();
         fileReader.getDataFromFile(file);
