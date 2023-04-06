2d1f50c9e05d2e801ef9c80499e736154ff0fcbf
Dzmitry Tsybulski <dzmitrytsybulski@gmail.com>
2023-04-06 13:25:33 +0300

    Create Stage 1 Task NIO Task 1

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,35 @@
 package com.epam.mjc.nio;
 
-import java.io.File;
-
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
+        try {
+            Path writeFilePath = Paths.get(String.valueOf(file));
+            String content = Files.readString(writeFilePath);
+
+            String[] subStr = content.split("\r\n");
+            String[][] subStrTwo = new String[subStr.length][2];
+            for (int i = 0; i < subStr.length; i++) {
+                subStrTwo[i] = subStr[i].split(": ");
+            }
+
+            return new Profile(subStrTwo[0][1], Integer.parseInt(subStrTwo[1][1]),
+                    subStrTwo[2][1], Long.valueOf(subStrTwo[3][1]));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return new Profile();
+    }
+
+    public static void main(String[] args) throws IOException {
+        File file = new File("src\\main\\resources\\Profile.txt");
+        FileReader fileReader = new FileReader();
+        fileReader.getDataFromFile(file);
     }
 }
